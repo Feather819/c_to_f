b9be36259ec6ed07f385ebf03184e17f7dc18030
Feather <s5648286819@gmail.com>
2020-02-11 17:41:01 +0800

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,6 @@
+#攝氏('C)轉華氏('F) F = C * 9/5 + 32
+
+temperature_C = input('請輸入攝氏溫度：')
+temperature_C = float(temperature_C)
+temperature_F = temperature_C * 9 / 5 + 32
+print('華氏溫度為：', temperature_F)
